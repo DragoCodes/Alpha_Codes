8e54cd15b90e548acf2dff89ef60c8cc081d5399
Drago <134218117+DragoCodes@users.noreply.github.com>
2024-07-03 18:41:55 +0530

    Add login authentication feature

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,24 @@ const User= require('./models/Users.js')
 const jwt= require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
+
+
 dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 // middlewares
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 DBConnection();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the today's class!");
+    res.send("REGISTRATION AND LOGIN");
 });
 
 app.post('/register', async(req, res) => {
@@ -32,7 +41,7 @@ app.post('/register', async(req, res) => {
         }
 
         // encrypt the password
-        const hashPassword = bcrypt.hashSync(password, 10);
+        const hashPassword = await bcrypt.hashSync(password, 10);
         console.log(hashPassword);
 
         // save the user in the database
@@ -45,21 +54,66 @@ app.post('/register', async(req, res) => {
 
         // generate token and send the response to the client   $$$$$$LEARN JWT TOKEN
         const token =jwt.sign({id:user._id,email}, process.env.SECRET_KEY,{
-            expiresIn: '1h'
+            expiresIn: '1d'
         });
         user.token = token;
         user.password = undefined;
-        res.status(201).json({
+
+        res.status(200).json({
             temp: "You have successfully registered!",
             user
         });
-        // console.log(token);
 
     }catch(error)   {
         console.error(error);
     }
 });
+app.post("/login", async (req, res) => {
+    try {
+        //get all the user data
+        const { email, password } = req.body;
 
-app.listen(8000,()=>{
-    console.log("Server is running on port 8000");
+        // check that all the data should exists
+        if (!(email && password)) {
+            return res.status(400).send("Please enter all the information");
+        }
+
+        //find the user in the database
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not found!");
+        }
+
+        //match the password
+        const enteredPassword = await bcrypt.compare(password, user.password);
+        if (!enteredPassword) {
+            return res.status(401).send("Password is incorrect");
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+            expiresIn: "1d",
+        });
+        user.token = token;
+        user.password = undefined;
+
+        //store cookies
+        const options = {
+            expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+            httpOnly: true, //only manipulate by server
+        };
+
+        //send the token
+        res.status(200).cookie("token", token, options).json({
+            message: "You have successfully logged in!",
+            success: true,
+            token,
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
